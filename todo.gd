7bd29f52ae406e6dc1a7d9dabd493fbb24c1cc79
velopman <74576289+velopman@users.noreply.github.com>
2021-01-28 22:39:22 -0500

    Day 1
    Add twitch chat
    Add player input
    Add maze
    Add initial art

---
+++ todo.gd
@@ -0,0 +1,23 @@
+# ggj ideas:
+# - theme: Lost and found
+# - diversifiers:
+#     - accessibility:
+#         - Spinal Tap (slider for all audio types)
+#     - art:
+#         - Are we ASCIIng too much? (ascii graphics)
+#         - Get to the point (pointillist style, shaders?)
+#     - code:
+#         - RNG (but for something typically static)
+#     - design:
+#         - Moving the goalposts (objective changes each time it's played)
+#         - How many of you are in there? (twitch chat?)
+#     - meta:
+#         - On the safe side (100km team)
+#         - A-Party (game for video call)
+
+# Chat based team maze escape game
+#	Each player joins the game and is assigned to a team
+#	Once the game starts they can input "up", "down", "left", "right" to move the character
+#		fuzzy search for terms "pu" "dwon"
+#		input.sort() <union> "down" && input.size() ~= "down".size()
+#	First team to escape the maze wins
